4a4d0fb57f76a35ee6db908eb36afc0549c52131
Iain Reid <iainreid382@gmail.com>
2018-08-03 22:04:45 +0000

    Clean comments

--- packages/micro-queue/lib/index.js
+++ packages/micro-queue/lib/index.js
@@ -1,18 +1,14 @@
-/* vim: set et ts=2 cc=120: */
-
 'use strict';
 
-/** Queue:
- *
+/**
  * This Array is simply responsible for the storage of the tasks. As tasks are added to the queue, they will be
  * retrieved by the executor in the order in which they were given, and removed from the queue upon completion.
  */
 const q = [];
 
-/** Executor:
- *
+/**
  * To improve the memory footprint of this micro-module, this entry point is abstracted to avoid the creation of an
- * unnecssary closure within in the task runner. 
+ * unnecssary closure within in the task runner.
  */
 const e = () => {
   // Register that the queue is being processed
@@ -22,8 +18,7 @@ const e = () => {
   return r();
 };
 
-/** Runner:
- *
+/**
  * The runner is a short, self executing function that will continue to self execute as long as there are tasks left
  * in the queue.
  */
