4def0ef11fc411655cc5da1af262f3a3f99a9040
Iain Reid <iainreid382@gmail.com>
2019-09-06 17:54:08 +0000

    Add centralised library to import all methods

---
+++ packages/micro/micro.js
@@ -0,0 +1,9 @@
+'use strict';
+
+module.exports = {
+  catch: require('@chaffity/micro-catch'),
+  curry: require('@chaffity/micro-curry'),
+  pause: require('@chaffity/micro-pause'),
+  queue: require('@chaffity/micro-queue'),
+  tap: require('@chaffity/micro-tap'),
+};
