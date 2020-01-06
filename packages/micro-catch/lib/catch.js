074d9d1c676f8259136eabb9c8e25c7c47a9d0b0
Iain Reid <iainreid382@gmail.com>
2020-01-06 16:49:54 +0000

    chore: Add documentation for catch module

--- packages/micro-catch/lib/catch.js
+++ packages/micro-catch/lib/catch.js
@@ -1,5 +1,16 @@
 'use strict';
 
+/**
+ * Safely invokes the provided function.
+ *
+ * @example
+ *
+ * M.catch(() => throw Error())
+ *
+ * @param fn - A function that may throw an error
+ *
+ * @returns The return value of `fn` or `null` is an error was thrown
+ */
 module.exports = fn => function () {
   try {
     return fn.apply(this, arguments)
