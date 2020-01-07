7a2a7248e227ffec4337b4596a86549d133a1422
Iain Reid <iainreid382@gmail.com>
2020-01-07 13:58:46 +0000

    chore: Add documentation for the tap module

--- packages/micro-tap/lib/tap.js
+++ packages/micro-tap/lib/tap.js
@@ -1,5 +1,16 @@
 'use strict';
 
+/**
+ * Access the value in a composition without consuming it.
+ *
+ * @example
+ *
+ * M.tap(console.log)("foo")
+ *
+ * @param fn - The function to be executed
+ *
+ * @returns A composable function
+ */
 module.exports = (fn) => (arg) => {
   fn(arg);
   return arg;
